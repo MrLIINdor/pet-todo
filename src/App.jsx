7a06066d965ab4286d438adc1eed941f6dc9296c
MrLIINdor <liindor54@mail.ru>
2023-10-07 23:55:45 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import "primereact/resources/themes/vela-green/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "./App.css";
-import { Button } from "primereact/button";
+import { TabMenu } from 'primereact/tabmenu';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const items = [
+    {label: 'Home', icon: 'pi pi-fw pi-home'},
+    {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
+    {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+    {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+    {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+];
 
-  return (
-    <div>
-      <Button>Click on the Vite and React logos to learn more</Button>
+return (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems:'center' }}>
+    <div className="card">
+        <TabMenu model={items} />
     </div>
-  );
+      
+    </div>
+  )
 }
 
 export default App;
